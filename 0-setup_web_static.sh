8b2eae6d702f24a145c88df06086fa25ffc630b0
LuisPatino92 <2450@holbertonschool.com>
2021-04-18 09:17:59 -0500

    Set up script tested ok in containers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# This script sets everything up to serve AirBnB content.
+
+# Install Nginx if it is not installed
+
+if ! [ -x "$(command -v nginx)" ]
+then
+
+	apt-get update -y;
+	apt-get upgrade -y;
+	apt-get install -y nginx;
+fi
+
+mkdir -p /data/web_static/releases/test/;
+mkdir -p /data/web_static/shared/;
+
+echo "<h1>FakeHTML for testing pourposes</h1>" >> /data/web_static/releases/test/index.html;
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current;
+
+chown -R ubuntu:ubuntu /data/;
+
+# Variables to put the location alias
+index_tester="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}";
+file_to_update="/etc/nginx/sites-enabled/default";
+
+# Checks if /etc/nginx/sites-enabled/default exists and updates it if so.
+if [ -f /etc/nginx/sites-enabled/default ]; then
+	grep "location /hbnb_static {" -q "${file_to_update}" || sed -i "/^\tserver_name _;$/a\ ${index_tester}" "${file_to_update}";
+fi
+
+service nginx status >> /dev/null || service nginx start;
+nginx -s reload;
