dee49c64ea9a717b4166c6b818958542dab4abd2
LuisPatino92 <2450@holbertonschool.com>
2021-05-02 16:05:37 -0500

    Using two routes binded to same function

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module has the minimum script to start running a web app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hbtn():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def parrot(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+@app.route('/python')
+@app.route('/python/<text>')
+def parrot_py(text='is_cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
