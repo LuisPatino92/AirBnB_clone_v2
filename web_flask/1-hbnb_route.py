83a4068f1042b77a529ded2f8222824685f3786f
LuisPatino92 <2450@holbertonschool.com>
2021-05-02 15:17:20 -0500

    Route for /hbnb added

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module has the minimum script to start running a web app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hbtn():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+if __name__ == "__main__":
+    app.run()
